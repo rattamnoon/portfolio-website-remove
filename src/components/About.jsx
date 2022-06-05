681f0950e4a01865b5df3b02fd1f98cfd2c550fe
rattamnoon <rattamnoon.kir@gmail.com>
2022-06-05 20:58:47 +0700

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,9 @@ const About = () => {
         </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
           <div className="sm:text-right text-4xl font-bold">
-            <p>Hi. I'm Clint, nice to meet you. Please take a look around.</p>
+            <p>
+              Hi. I'm Rattamnoon, nice to meet you. Please take a look around.
+            </p>
           </div>
           <div>
             <p>
