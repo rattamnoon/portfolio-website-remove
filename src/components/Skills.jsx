e4448de2d369181402b59deab043c1c5754e36dd
rattamnoon <rattamnoon.kir@gmail.com>
2022-06-05 22:22:25 +0700

    first commit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -35,7 +35,7 @@ const dataSource = [
 
 const Skills = () => {
   return (
-    <div name="skills" className="w-full h-full bg-[#0a192f] text-gray-300">
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p className="text-4xl font-bold inline border-b-4 border-pink-600">
