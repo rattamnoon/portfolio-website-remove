cf5d6f26143d0f38ae10483dceacc1cb779b71b2
rattamnoon <rattamnoon.kir@gmail.com>
2022-06-11 13:46:05 +0700

    update

--- src/config/mainUrl.js
+++ src/config/mainUrl.js
@@ -1,4 +1,4 @@
 const currentUrl =
-  process.env.NODE_ENV === 'production' ? 'process' : 'http://localhost:4000';
+  process.env.NODE_ENV === 'production' ? 'process' : 'http://localhost:3001';
 
 export default currentUrl;
